296e00a3235ff33208f0ad158d2d12d90c73b574
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-07-10 23:56:55 -0300

    Reindent some files and clear init file

--- imdb_crawler/spiders/imdb.py
+++ imdb_crawler/spiders/imdb.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from scrapy            import Spider
+from scrapy import Spider
 from imdb_crawler.items import MovieItem
 
 class ImdbSpider(Spider):
-    name = 'imdb'
-    allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/movies-coming-soon/?ref_=inth_cs',]
+  name = 'imdb'
+  allowed_domains = ['imdb.com']
+  start_urls = ['https://www.imdb.com/movies-coming-soon/?ref_=inth_cs',]
 
-    def parse(self, response):
-        for movie in response.css('div.list_item'):
-            item = MovieItem()
+  def parse(self, response):
+    for movie in response.css('div.list_item'):
+      item = MovieItem()
 
-            item['title']       = movie.css('h4 a::text').extract_first()
-            item['year']        = item['title']
-            item['genre']       = movie.css('.cert-runtime-genre span:not([class^="ghost"])::text').extract()
-            item['stars']       = movie.css('.txt-block')[1].css('a::text').extract()
-            item['director']    = movie.css('.txt-block a::text').extract_first()
-            item['duration']    = movie.css('time::text').extract_first()
-            item['metascore']   = movie.css('span.metascore::text').extract_first()
-            item['description'] = movie.css('.outline::text').extract_first().strip()
+      item['title'] = movie.css('h4 a::text').extract_first()
+      item['year'] = item['title']
+      item['genre'] = movie.css('.cert-runtime-genre span:not([class^="ghost"])::text').extract()
+      item['stars'] = movie.css('.txt-block')[1].css('a::text').extract()
+      item['director'] = movie.css('.txt-block a::text').extract_first()
+      item['duration'] = movie.css('time::text').extract_first()
+      item['metascore'] = movie.css('span.metascore::text').extract_first()
+      item['description'] = movie.css('.outline::text').extract_first().strip()
 
-            item['image_urls']  = movie.css('img.poster::attr(src)').extract()
+      item['image_urls'] = movie.css('img.poster::attr(src)').extract()
 
-            yield item
+      yield item
